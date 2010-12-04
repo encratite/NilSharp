3914b98d3ac03f6888d6d911fe62f4868020bbd2
void <git.email.address@googlemail.com>
2010-12-04 23:18:37 +0100

    Added some code for file operations

---
+++ Nil/File.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Nil
+{
+	public class File
+	{
+		public enum FileType
+		{
+			File,
+			Directory,
+			DoesNotExist,
+		}
+
+		public static FileType GetFileType(string path)
+		{
+			try
+			{
+				FileAttributes attributes = System.IO.File.GetAttributes(path);
+				return
+					(attributes & FileAttributes.Directory) != 0 ?
+					FileType.Directory :
+					FileType.File;
+			}
+			catch (FileNotFoundException)
+			{
+				return FileType.DoesNotExist;
+			}
+			catch (DirectoryNotFoundException)
+			{
+				return FileType.DoesNotExist;
+			}
+		}
+	}
+}
