55e1c1ccb441ae88330458b5a045b38218282789
void <git.email.address@googlemail.com>
2010-12-15 07:53:07 +0100

    Fixed the offset issues with the form state

--- Nil/FormState.cs
+++ Nil/FormState.cs
@@ -24,11 +24,27 @@ namespace Nil
 			WindowOffset = loadForm.Location;
 		}
 
+		bool IsSane()
+		{
+			int
+				x = WindowOffset.X,
+				y = WindowOffset.Y;
+
+			return
+			!(
+				x < 0 || y < 0 ||
+				x > Screen.PrimaryScreen.WorkingArea.Width || y > Screen.PrimaryScreen.WorkingArea.Height
+			);
+		}
+
 		public void Apply(Form applyForm)
 		{
-			applyForm.WindowState = Maximised ? FormWindowState.Maximized : FormWindowState.Normal;
-			applyForm.Size = WindowSize;
-			applyForm.Location = WindowOffset;
+			if (IsSane())
+			{
+				applyForm.WindowState = Maximised ? FormWindowState.Maximized : FormWindowState.Normal;
+				applyForm.Size = WindowSize;
+				applyForm.Location = WindowOffset;
+			}
 		}
 	}
 }
