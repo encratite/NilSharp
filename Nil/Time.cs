f7d8dce238dab98d24cd28af62a8f9adcc154952
void <git.email.address@googlemail.com>
2011-04-26 16:47:23 +0200

    Added a function for timestamp strings

---
+++ Nil/Time.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nil
+{
+	public class Time
+	{
+		public static string Timestamp()
+		{
+			return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+		}
+	}
+}
