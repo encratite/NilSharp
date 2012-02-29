9dac2830e684862704716bbd4ebad7c65c5d66b1
void <git.email.address@googlemail.com>
2012-02-29 17:42:11 +0100

    Added synchronised console output

---
+++ Nil/Output.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nil
+{
+	public class Output
+	{
+		static Object consoleLock = new Object();
+
+		public static void Write(string input)
+		{
+			lock (consoleLock)
+			{
+				Console.Write(input);
+			}
+		}
+
+		public static void WriteLine(string input)
+		{
+			Write(input + "\n");
+		}
+	}
+}
