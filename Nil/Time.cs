1244fc50a1c544eb526677b59bf66747c29b1c43
void <git.email.address@googlemail.com>
2012-03-21 18:29:51 +0100

    Added some methods for Unix time handling

--- Nil/Time.cs
+++ Nil/Time.cs
@@ -4,6 +4,8 @@ namespace Nil
 {
 	public static class Time
 	{
+		static DateTime Epoch = new DateTime(1970, 1, 1);
+
 		public static string Timestamp()
 		{
 			return DateTime.Now.ToStandardString();
@@ -12,6 +14,21 @@ namespace Nil
 		public static string ToStandardString(this DateTime time)
 		{
 			return time.ToString("yyyy-MM-dd HH:mm:ss");
+		}
+
+		public static long ToUnixTime(this DateTime time)
+		{
+			return (long)(time - Epoch).TotalSeconds;
+		}
+
+		public static DateTime FromUnixTime(this long timestamp)
+		{
+			return Epoch.AddSeconds(timestamp);
+		}
+
+		public static long UnixTime()
+		{
+			return DateTime.UtcNow.ToUnixTime();
 		}
 	}
 }
